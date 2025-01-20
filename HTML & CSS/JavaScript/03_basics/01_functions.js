5db100de9f18e5c1b435eb1d6c9fcedec7cfb10d
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-01-20 21:48:48 +0530

    20/01/2025

---
+++ HTML & CSS/JavaScript/03_basics/01_functions.js
@@ -0,0 +1,17 @@
+
+function sayMyName() {
+    console.log("P");
+    console.log("r");
+    console.log("a");
+    console.log("g");
+    console.log("y");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2) {
+    console.log(number1 + number2)
+}
