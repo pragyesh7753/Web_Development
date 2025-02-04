3b38b1ec5e6fe1d2237d1ce171511d98f9a0d8ce
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-02-05 00:09:30 +0530

    05/02/2025

---
+++ JavaScript/03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+// var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    // console.log("INNER: ", a);
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one() {
+    const username = "pragyesh"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+}
