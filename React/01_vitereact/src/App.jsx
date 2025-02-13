3c21c636315311a018858c07492056148091cc39
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-02-13 11:12:42 +0530

    13/02/2025

---
+++ React/01_vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+// <> </> is a fragment, it is used to wrap multiple elements
+function App() {
+  return (
+    <>
+      <Chai />
+      <p>test para</p>
+    </>
+  )
+}
+
+export default App
