2227ac990389f237cfe998f740b000be9848b337
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-02-14 23:59:44 +0530

    14/02/2025

--- JavaScript/07_projects/04-GuessTheNumber/script.js
+++ JavaScript/07_projects/04-GuessTheNumber/script.js
@@ -1,1 +1,51 @@
-Math.random()
+const randomNumber = parseInt(Math.random() * 100 + 1)
+
+const submit = document.querySelector("#subt")
+const userInput = document.querySelector("#guessField")
+const guessSlot = document.querySelector(".guesses")
+const remaining = document.querySelector(".lastResult")
+const lowOrHi = document.querySelector(".lowOrHi")
+const startOver = document.querySelector(".resultParas")
+
+const p = document.createElement('p')
+
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+if (playGame) {
+    submit.addEventListener("click", function (e) {
+        e.preventDefault()
+        const guess = parseInt(userInput.value)
+        validateGuess(guess)
+    })
+}
+
+function validateGuess(guess) {
+    if (NaN(guess)) {
+        alert("Please enter a valid number")
+
+    }
+}
+
+function checkGuess(guess) {
+
+}
+
+function displayGuess(guess) {
+
+}
+
+function displayMessage(message) {
+
+}
+
+function endGame() {
+
+}
+
+function newGame() {
+
+}
