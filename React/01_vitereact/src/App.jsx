03a25ec3f2a27860466f406b33bdafe82fc4e6f7
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-02-16 10:01:27 +0530

    16/02/2025

--- React/01_vitereact/src/App.jsx
+++ React/01_vitereact/src/App.jsx
@@ -2,9 +2,12 @@ import Chai from "./chai"
 
 // <> </> is a fragment, it is used to wrap multiple elements
 function App() {
+  const userName = "chai aur code"
+
   return (
     <>
       <Chai />
+      <h1>chai aur react {userName}</h1>
       <p>test para</p>
     </>
   )
