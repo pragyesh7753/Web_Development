a17f9907a9c7abf75ad32f87e4bd3396953c19ab
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-03-09 09:23:33 +0530

    08/03/2025

--- React/08-miniContext/src/App.jsx
+++ React/08-miniContext/src/App.jsx
@@ -1,12 +1,18 @@
 
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
-  
+
 
   return (
-    <>
-    <h1>React With Chai</h1>
-    </>
+    <UserContextProvider>
+      <h1>React With Chai</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
