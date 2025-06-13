1f63e81c1f32c0b9a217a5dd7c88e93dc0ca5994
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-06-13 16:08:11 +0530

    13/06/2025

--- Backend/Mini-Project-2/app.js
+++ Backend/Mini-Project-2/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const userModel = require('./models/user');
+const cookieParser = require('cookie-parser');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
     console.log("hey");
 });
-app.post('/create', (req, res) => {
-    res.send('Hello World!');
-});
+
 
 app.listen(3000)
