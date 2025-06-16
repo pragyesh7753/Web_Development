309f99979d66f9643b6d84af17621dc89c006d4d
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-06-16 17:34:55 +0530

    16/06/2025

--- Backend/Mini-Project-2/app.js
+++ Backend/Mini-Project-2/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const userModel = require('./models/user');
 const cookieParser = require('cookie-parser');
+const user = require('./models/user');
+const bcrypt = require('bcrypt');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -10,6 +12,25 @@ app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.render("index")
+});
+
+app.post('/register', async (req, res) => {
+    let { email, username, name, age, password } = req.body;
+    let user = await userModel.findOne({ email })
+
+    if (user) return res.status(500).send("User already registered")
+
+    bcrypt.genSalt(10, async (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            await userModel.create({
+                username,
+                email,
+                age,
+                name,
+                password: hash
+            })
+        })
+    })
 });
 
 
