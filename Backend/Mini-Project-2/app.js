0d9fcea391b3e6b08aac2155631df0e12b458414
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-06-17 18:23:51 +0530

    17/06/2025

--- Backend/Mini-Project-2/app.js
+++ Backend/Mini-Project-2/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const userModel = require('./models/user');
+const postModel = require('./models/post');
 const cookieParser = require('cookie-parser');
 const user = require('./models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -20,18 +22,62 @@ app.post('/register', async (req, res) => {
 
     if (user) return res.status(500).send("User already registered")
 
-    bcrypt.genSalt(10, async (err, salt) => {
+    bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(password, salt, async (err, hash) => {
-            await userModel.create({
+            let user = await userModel.create({
                 username,
                 email,
                 age,
                 name,
                 password: hash
             })
+
+            let token = jwt.sign({ email: email, userid: user._id }, "shhh")  // "shhh" is a secret key"
+            res.cookie("token", token)
+            res.send("Registered successfully")
         })
+    });
+});
+
+app.post('/login', async (req, res) => {
+    let { email, password } = req.body;
+    let user = await userModel.findOne({ email })
+
+    if (!user) return res.status(500).send("Something went wrong")
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+            let token = jwt.sign({ email: email, userid: user._id }, "shhh")  // "shhh" is a secret key"
+            res.cookie("token", token)
+            res.redirect("/profile")
+        }
+        else res.redirect("/login")
+
     })
 });
 
+app.get("/login", (req, res) => {
+    res.render("login")
+});
+
+app.get("/profile", isLoggedIn, async (req, res) => {
+    // console.log(req.user);
+    let user = await userModel.findOne({ email: req.user.email })
+    // console.log(user);
+    res.render("profile", { user });
+});
+
+app.get("/logout", (req, res) => {
+    res.cookie("token", "")
+    res.render("login")
+});
+
+function isLoggedIn(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) return res.redirect("/login");
+    let data = jwt.verify(token, "shhh");
+    req.user = data;
+    next();
+}
 
 app.listen(3000)
