de4ec05c70503e07d7881948f3961f754a2bfb23
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-06-22 23:10:47 +0530

    Add URL parameter to email verification template

--- Major Projects/Blinkeyit/server/utils/verifyEmailTemplate.js
+++ Major Projects/Blinkeyit/server/utils/verifyEmailTemplate.js
@@ -1,2 +1,8 @@
-const verifyEmailTemplate = (name,) => {
+const verifyEmailTemplate = (name, url) => {
+    return `
+    <p>Thank you for registering on Blinkeyit.</p>
+    <button style = "color : white; background : blue ; margin-top : 10px>
+    Verify Email
+    </button>
+    `
 }
